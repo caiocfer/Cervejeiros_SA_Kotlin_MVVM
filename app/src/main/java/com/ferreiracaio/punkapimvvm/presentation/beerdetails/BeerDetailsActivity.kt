14c2df07a8d9fb5be19791728aa9df9151cd2969
Caio Cesar <caiocfer@gmail.com>
2021-08-21 02:11:42 -0300

    Finished BeerDetailsActivity

--- app/src/main/java/com/ferreiracaio/punkapimvvm/presentation/beerdetails/BeerDetailsActivity.kt
+++ app/src/main/java/com/ferreiracaio/punkapimvvm/presentation/beerdetails/BeerDetailsActivity.kt
@@ -1,12 +1,11 @@
 package com.ferreiracaio.punkapimvvm.presentation.beerdetails
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.ferreiracaio.punkapimvvm.R
 import com.ferreiracaio.punkapimvvm.data.response.BeerResponseItem
+import com.ferreiracaio.punkapimvvm.data.response.Malt
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_beer_details.*
 
 class BeerDetailsActivity : AppCompatActivity() {
@@ -15,11 +14,39 @@ class BeerDetailsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_beer_details)
 
         val beer = intent.extras?.get("BEER_DETAILS") as BeerResponseItem
-        setSupportActionBar(beerDetailToolbar).apply {
+        setSupportActionBar(beerDetailsToolbar).apply {
             title = beer.name
             titleColor = R.color.white
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        Picasso.get().load(beer.imageUrl).into(imageBeerDetails)
+        beerDetailsDescription.text = beer.description
+        var malt: String = ""
+        var hop:String = ""
+
+
+        for(malts in beer.ingredients.malt){
+            malt += "${malts.name} \n"
+            malt += "Amount: ${malts.amount.value} "
+            malt += "${malts.amount.unit}\n"
+        }
+
+        val hopList = beer.ingredients.hops
+        for(hops in hopList){
+            hop += "${hops.name}\n"
+            hop += "${hops.amount.value} "
+            hop += "${hops.amount.unit}\n"
+            hop += "Add: ${hops.add}\n"
+            hop += "Attribute: ${hops.attribute}\n"
+
+        }
+
+        beerDetailsMalt.text = malt
+        beerDetailsHops.text = hop
+        beerDetailsYeast.text = beer.ingredients.yeast
+
+
 
     }
 }
