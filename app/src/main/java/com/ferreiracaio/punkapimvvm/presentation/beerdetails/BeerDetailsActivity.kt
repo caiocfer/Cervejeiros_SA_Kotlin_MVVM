5a6b559200018ddc3e3cea2a32bb3ac320926df3
Caio Cesar <caiocfer@gmail.com>
2021-08-20 22:58:48 -0300

    Added toolbar in BeerDetailsActivity

--- app/src/main/java/com/ferreiracaio/punkapimvvm/presentation/beerdetails/BeerDetailsActivity.kt
+++ app/src/main/java/com/ferreiracaio/punkapimvvm/presentation/beerdetails/BeerDetailsActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.ferreiracaio.punkapimvvm.R
 import com.ferreiracaio.punkapimvvm.data.response.BeerResponseItem
+import kotlinx.android.synthetic.main.activity_beer_details.*
 
 class BeerDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +15,11 @@ class BeerDetailsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_beer_details)
 
         val beer = intent.extras?.get("BEER_DETAILS") as BeerResponseItem
-        Log.d("TAG", "onCreate: ${beer.name}")
+        setSupportActionBar(beerDetailToolbar).apply {
+            title = beer.name
+            titleColor = R.color.white
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
     }
 }
