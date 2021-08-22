7edd068147756fc0c675be51f2121d7fee22e1c6
Caio Cesar <caiocfer@gmail.com>
2021-08-22 13:09:05 -0300

    Made a few code adjustments

--- app/src/main/java/com/ferreiracaio/punkapimvvm/presentation/beerlist/BeerListFragment.kt
+++ app/src/main/java/com/ferreiracaio/punkapimvvm/presentation/beerlist/BeerListFragment.kt
@@ -1,11 +1,9 @@
 package com.ferreiracaio.punkapimvvm.presentation.beerlist
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -15,31 +13,33 @@ import kotlinx.android.synthetic.main.beer_list_fragment.*
 class BeerListFragment : Fragment(R.layout.beer_list_fragment) {
 
     private lateinit var viewModel: BeerListViewModel
+    private lateinit var beerAdapter: BeerListAdapter
     var page:Int=1
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view,savedInstanceState)
         viewModel = ViewModelProvider(this).get(BeerListViewModel::class.java)
-        val adapter = BeerListAdapter(viewModel.beerList)
+        beerAdapter = BeerListAdapter(viewModel.beerList)
 
         val linearLayoutManager: LinearLayoutManager = LinearLayoutManager(requireContext(),RecyclerView.VERTICAL,false)
-        recyclerBeerList.layoutManager = linearLayoutManager
-        recyclerBeerList.setHasFixedSize(true)
-        recyclerBeerList.adapter = adapter
 
-        viewModel.beersLiveData.observe(viewLifecycleOwner,{beerList->
-            beerList.let {
-                adapter.notifyItemRangeInserted(viewModel.beerList.size,25)
-            }
-        })
+        recyclerBeerList.apply {
+            layoutManager = linearLayoutManager
+            setHasFixedSize(true)
+            adapter = beerAdapter
+        }
+
+
+
+        observeLoadingState()
+        observeBeerList()
         viewModel.getBeers()
-
 
         searchBeer.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
             override fun onQueryTextSubmit(query: String?): Boolean {
                 if (query != null) {
                     viewModel.getBeerSearch(query)
-                    adapter.notifyDataSetChanged()
+                    beerAdapter.notifyDataSetChanged()
                 }
                 return true
             }
@@ -47,19 +47,13 @@ class BeerListFragment : Fragment(R.layout.beer_list_fragment) {
             override fun onQueryTextChange(newText: String?): Boolean {
                 return false
             }
-
-
-
         })
 
-        searchBeer.setOnCloseListener(object: SearchView.OnCloseListener{
-            override fun onClose(): Boolean {
-                viewModel.getBeers()
-                adapter.notifyDataSetChanged()
-                return true
-            }
-
-        })
+        searchBeer.setOnCloseListener {
+            viewModel.getBeers()
+            beerAdapter.notifyDataSetChanged()
+            true
+        }
 
         recyclerBeerList.addOnScrollListener(object : RecyclerView.OnScrollListener(){
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
@@ -73,13 +67,24 @@ class BeerListFragment : Fragment(R.layout.beer_list_fragment) {
                         recyclerBeerList.post {
                             page += 1
                             viewModel.loadNextPage(page)
-                            adapter.notifyItemRangeInserted(viewModel.beerList.size,25)
+                            beerAdapter.notifyItemRangeInserted(viewModel.beerList.size,25)
                         }
                     }
                 }
             }
         })
+    }
 
+    private fun observeBeerList(){
+        viewModel.beersLiveData.observe(viewLifecycleOwner,{beerList->
+            beerList.let {
+                beerAdapter.notifyItemRangeInserted(viewModel.beerList.size,25)
+            }
+        })
+        viewModel.getBeers()
+    }
+
+    private fun observeLoadingState(){
         viewModel.isLoading.observe(viewLifecycleOwner,{isLoading->
             if(isLoading){
                 progressBeerList.visibility = View.VISIBLE
@@ -87,7 +92,6 @@ class BeerListFragment : Fragment(R.layout.beer_list_fragment) {
                 progressBeerList.visibility = View.GONE
             }
         })
-
     }
 
 
